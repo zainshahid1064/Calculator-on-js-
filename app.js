3b760a6cb4e3768e828d0c4c3a52eab88db146f0
zainshahid1064 <zs9368952@gmail.com>
2025-07-17 13:09:56 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+//Addition
+var fNumber = +prompt("Enter your first desire number to Add");
+var sNumber = +prompt("Enter your second desire number to Add");
+alert("Your Ansewer is: " + (fNumber + sNumber));
+
+//Subtraction
+var fNumber = +prompt("Enter your first desire number to Substract");
+var sNumber = +prompt("Enter your second desire number to Subtract");
+alert("Your Ansewer is: " + (fNumber - sNumber));
+
+//Multiplication
+var fNumber = +prompt("Enter your first desire number to Multiply");
+var sNumber = +prompt("Enter your second desire number to Multiply");
+alert("Your Ansewer is: " + fNumber * sNumber);
+
+//Division
+var fNumber = +prompt("Enter your first desire number to Divide");
+var sNumber = +prompt("Enter your second desire number to Divide");
+alert("Your Ansewer is: " + fNumber / sNumber);
